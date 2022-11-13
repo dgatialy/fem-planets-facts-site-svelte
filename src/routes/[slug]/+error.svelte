b29a5f67efc33ca9f73b1c3d08c36ea1e1650500
dgatialy <89516674+dgatialy@users.noreply.github.com>
2022-11-13 09:28:56 +0100

    Create +error.svelte

---
+++ src/routes/[slug]/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Header from '../../stories/planets-facts/Header.svelte';
+	import { page } from '$app/stores';		
+	
+</script>
+
+<div data-theme="earth" class="theme">
+	<Header />
+
+	<main>
+		<h1>{$page.status}</h1>
+        <h2>{$page.error?.message}</h2>
+	</main>
+</div>
